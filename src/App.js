fa0354d9a1ee19d9863629d27bb4c08247389346
LorikHasani <kikpubgm@gmail.com>
2023-07-02 12:09:56 +0200

    Fetching data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import { children, useEffect, useState } from "react";
-
-// const weatherData = [
-//   {
-//     cityId: "tt1375666",
-//     Name: "London",
-//     Temp: 37,
-//     Description: "Sunny",
-//   },
-// ];
+import { useEffect, useState } from "react";
 
 const KEY = "3bab8c719a204ddfc61a21dcd1f89512";
 
 export default function App() {
-  const [cities, setCities] = useState([]);
+  const [cities, setCities] = useState({});
   const [query, setQuery] = useState("London");
   const [error, setError] = useState("");
 
@@ -32,17 +23,15 @@ export default function App() {
             throw new Error("Something went wrong with fetching city");
 
           const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not found");
+          if (data.cod !== 200) throw new Error("City not found");
 
-          setCities(data.Search);
+          setCities(data);
           setError("");
         } catch (err) {
           if (err.name !== "AbortError") {
             console.error(err.message);
             setError(err.message);
           }
-        } finally {
-          // setIsLoading(false);
         }
       }
 
@@ -89,13 +78,13 @@ function Search({ query, setQuery }) {
 function Weather({ cities }) {
   return (
     <div className="cardContainer">
-      {cities?.map((city) => (
+      {cities.main && (
         <div className="card">
-          <p class="city">{city.main.temp}</p>
-          {/* <p class="weather">{city.lat}</p> */}
-          {/* <p class="temp">{city.main}</p> */}
+          <p className="city">{cities.main.temp}</p>
+          <p className="weather">{cities.weather[0].main}</p>
+          <p className="temp">{cities.weather[0].description}</p>
         </div>
-      ))}
+      )}
     </div>
   );
 }
