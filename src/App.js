edc99b44548952ba2bbb86759ed4f80393b44a87
LorikHasani <kikpubgm@gmail.com>
2023-07-02 12:08:47 +0200

    fetchin data

--- src/App.js
+++ src/App.js
@@ -1,40 +1,69 @@
 import { children, useEffect, useState } from "react";
 
-const weatherData = [
-  {
-    cityId: "tt1375666",
-    Name: "London",
-    Temp: 37,
-    Description: "Sunny",
-  },
-];
+// const weatherData = [
+//   {
+//     cityId: "tt1375666",
+//     Name: "London",
+//     Temp: 37,
+//     Description: "Sunny",
+//   },
+// ];
 
 const KEY = "3bab8c719a204ddfc61a21dcd1f89512";
 
 export default function App() {
-  const [cities, setCities] = useState(weatherData);
+  const [cities, setCities] = useState([]);
+  const [query, setQuery] = useState("London");
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      const controller = new AbortController();
+
+      async function fetchWeather() {
+        try {
+          setError("");
+          const res = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${KEY}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching city");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found");
+
+          setCities(data.Search);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            console.error(err.message);
+            setError(err.message);
+          }
+        } finally {
+          // setIsLoading(false);
+        }
+      }
+
+      fetchWeather();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
 
   return (
     <div className="container">
       <Header>
-        <Search />
+        <Search query={query} setQuery={setQuery} />
       </Header>
       <Weather cities={cities} />
     </div>
   );
 }
-
-useEffect(function () {
-  async function fetchWeather() {
-    const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${KEY}`
-    );
-
-    const data = await res.json();
-  }
-
-  fetchWeather();
-});
 
 function Header({ children }) {
   return (
@@ -45,9 +74,7 @@ function Header({ children }) {
   );
 }
 
-function Search() {
-  const [query, setQuery] = useState("");
-
+function Search({ query, setQuery }) {
   return (
     <input
       className="search"
@@ -63,10 +90,10 @@ function Weather({ cities }) {
   return (
     <div className="cardContainer">
       {cities?.map((city) => (
-        <div className="card" key={city.cityId}>
-          <p class="city">{city.Name}</p>
-          <p class="weather">{city.Description}</p>
-          <p class="temp">32°</p>
+        <div className="card">
+          <p class="city">{city.main.temp}</p>
+          {/* <p class="weather">{city.lat}</p> */}
+          {/* <p class="temp">{city.main}</p> */}
         </div>
       ))}
     </div>
