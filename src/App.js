ba1dce3ff2743028a0704abab1c274f03a51cf07
LorikHasani <kikpubgm@gmail.com>
2023-07-02 10:59:40 +0200

    Weather App - First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { children, useEffect, useState } from "react";
 
-function App() {
+const weatherData = [
+  {
+    cityId: "tt1375666",
+    Name: "London",
+    Temp: 37,
+    Description: "Sunny",
+  },
+];
+
+const KEY = "3bab8c719a204ddfc61a21dcd1f89512";
+
+export default function App() {
+  const [cities, setCities] = useState(weatherData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header>
+        <Search />
+      </Header>
+      <Weather cities={cities} />
     </div>
   );
 }
 
-export default App;
+useEffect(function () {
+  async function fetchWeather() {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${KEY}`
+    );
+
+    const data = await res.json();
+  }
+
+  fetchWeather();
+});
+
+function Header({ children }) {
+  return (
+    <div className="header">
+      <h3>Weather App</h3>
+      {children}
+    </div>
+  );
+}
+
+function Search() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Enter the city..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+
+function Weather({ cities }) {
+  return (
+    <div className="cardContainer">
+      {cities?.map((city) => (
+        <div className="card" key={city.cityId}>
+          <p class="city">{city.Name}</p>
+          <p class="weather">{city.Description}</p>
+          <p class="temp">32°</p>
+        </div>
+      ))}
+    </div>
+  );
+}
