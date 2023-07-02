e49a6a80401f94af72e75087e2600302511ecc67
LorikHasani <kikpubgm@gmail.com>
2023-07-02 21:52:23 +0200

    Added Loading

--- src/App.js
+++ src/App.js
@@ -4,55 +4,76 @@ const KEY = "3bab8c719a204ddfc61a21dcd1f89512";
 
 export default function App() {
   const [cities, setCities] = useState({});
-  const [query, setQuery] = useState("London");
+  const [query, setQuery] = useState("-");
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false); // Set initial state to true
 
-  useEffect(
-    function () {
-      const controller = new AbortController();
+  useEffect(() => {
+    const controller = new AbortController();
 
-      async function fetchWeather() {
-        try {
-          setError("");
-          const res = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${KEY}`,
-            { signal: controller.signal }
-          );
+    async function fetchWeather() {
+      try {
+        setError("");
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${KEY}`,
+          { signal: controller.signal }
+        );
 
-          if (!res.ok)
-            throw new Error("Something went wrong with fetching city");
-
-          const data = await res.json();
-          if (data.cod !== 200) throw new Error("City not found");
-
-          setCities(data);
-          setError("");
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            console.error(err.message);
-            setError(err.message);
-          }
+        if (!res.ok) {
+          throw new Error("Something went wrong with fetching city");
         }
+
+        const data = await res.json();
+        if (data.cod !== 200) {
+          throw new Error("City not found");
+        }
+
+        setCities(data);
+        setError("");
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          console.error(err.message);
+          setError(err.message);
+        }
+      } finally {
+        setIsLoading(false);
       }
+    }
 
-      fetchWeather();
+    fetchWeather();
 
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
-  );
+    return function () {
+      controller.abort();
+    };
+  }, [query]);
 
   return (
     <div className="container">
       <Header>
         <Search query={query} setQuery={setQuery} />
       </Header>
-      <Weather cities={cities} />
+      {isLoading || Object.keys(cities).length === 0 ? (
+        <Loader />
+      ) : (
+        <Weather cities={cities} />
+      )}
+
+      {/* {error && <ErrorMessage message={error} />} */}
     </div>
   );
 }
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
+}
+
+// function ErrorMessage({ message }) {
+//   return (
+//     <p className="error">
+//       <span>❌</span> {message}
+//     </p>
+//   );
+// }
 
 function Header({ children }) {
   return (
@@ -80,9 +101,9 @@ function Weather({ cities }) {
     <div className="cardContainer">
       {cities.main && (
         <div className="card">
-          <p className="city">{cities.main.temp}</p>
-          <p className="weather">{cities.weather[0].main}</p>
-          <p className="temp">{cities.weather[0].description}</p>
+          <p className="city">{cities.name}</p>
+          <p className="city">{cities.main.temp} °F</p>
+          <p className="weather">{cities.weather[0].description}</p>
         </div>
       )}
     </div>
